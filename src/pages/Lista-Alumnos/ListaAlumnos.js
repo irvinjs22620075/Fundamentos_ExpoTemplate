4a2f529971a684cc4467c466166ae4a7d53e65f9
irvinjs22620075 <22620075@tlaxiaco.tecnm.mx>
2025-09-12 23:23:56 -0600

    Agregando Appbar e icono

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,20 +1,42 @@
-import { StyleSheet, View, Platform, StatusBar, Text} from "react-native";
+import { StyleSheet, View, Platform, StatusBar, Text, SafeAreaView, } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
  
  
  export default function ListaAlumnos() {
    return (
-   <View style={style.mainS}>
-    <Text>Hola</Text>
-   </View>
-   );
+   <SafeAreaView style={style.mainS}>
+    {/* Inicio de Appbar */}
+    <View style={style.appBar}>
+    <Icon name ="arrow-left" size={30} color={'#000'} />
+    <Text style={style.appBarTitle}>Lista de Usuarios</Text>
+    <View style={{width:20}}>
+
+    </View>
+    </View>
+    {/* Cierre de Appbar */}
+   </SafeAreaView>
+   )
  }
  const style = StyleSheet.create({
      mainS: {
          flex: 1,
-         backgroundColor: '#adccbfff',
+         backgroundColor: '#ffffffff',
          paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
-         padding: 16,
+        //  padding: 16,
+     },
+     appBar:{
+        height:50,
+        width:'100%',
+        backgroundColor:'#7bbd94ff',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        paddingHorizontal:16,
+     },
+     appBarTitle:{
+        fontWeight:'bold',
+        fontSize:16,
      },
 
     });
